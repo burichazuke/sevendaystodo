a3f7bec71dff5555c820d787c6b9291498f6d044
terakado <sakusaku.sasuke0426@gmail.com>
2019-08-23 16:07:29 +0900

    update

--- app/assets/javascripts/check.js
+++ app/assets/javascripts/check.js
@@ -1,6 +1,10 @@
 $(document).on('turbolinks:load', function(){
+  var html = `<img class="complete" src="/assets/nenga_hanko_1566540704707-c70421dd37f8d2349bdffddb19ec899a0fd50d0d31636b1e5463a720b01f0a53.png">`
+
   $(document).on('click','.check-box', function(){
     var target = $(this)
+    var parent = $(this).parent().parent()
+    var brother = $(parent).prev()
     var id = $(target).data()
     var url = `todos/${id.id}`
     $.ajax({
@@ -12,6 +16,7 @@ $(document).on('turbolinks:load', function(){
     .done(function(){
       $(target).removeClass('fa-square-o check-box')
       $(target).addClass('fa-check-square-o checked-box')
+      $(brother).append(html)
     })
     .fail(function(){
       console.log("失敗")
@@ -20,6 +25,9 @@ $(document).on('turbolinks:load', function(){
 
   $(document).on('click','.checked-box', function(){
     var target = $(this)
+    var parent = $(this).parent().parent()
+    var brother = $(parent).prev()
+    var child = $(brother).children().last()
     var id = $(target).data()
     var url = `todos/${id.id}`
     $.ajax({
@@ -31,6 +39,7 @@ $(document).on('turbolinks:load', function(){
     .done(function(){
       $(target).removeClass('fa-check-square-o checked-box')
       $(target).addClass('fa-square-o check-box')
+      $(child).remove()
     })
     .fail(function(){
       console.log("失敗")
